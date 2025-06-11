2a974446faa341af3a1983e03ba772c1b5baf5a2
Jordan Cowan <130715706+JoCo04@users.noreply.github.com>
2025-06-11 12:01:29 -0700

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,40 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "minecraft" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name      = "minecraft_key.pem"
+  associate_public_ip_address = true
+
+  vpc_security_group_ids = [aws_security_group.minecraft_sg.id]
+
+  tags = {
+    Name = "minecraft-server"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > public_ip.txt"
+  }
+}
+
+resource "aws_security_group" "minecraft_sg" {
+  name        = "minecraft-sg"
+  description = "Allow Minecraft port"
+
+  ingress {
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
